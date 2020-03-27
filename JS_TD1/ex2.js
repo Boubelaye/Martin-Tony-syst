3dc4436213806d37554f4bec238894abab1e52be
Tony Martin <tonym_10@hotmail.fr>
2020-03-27 12:36:52 +0100

    début exercice 2

---
+++ JS_TD1/ex2.js
@@ -0,0 +1,20 @@
+'use strict'
+
+
+function creerMultiplicateur(n){
+	return(x) => x*n;
+}
+
+
+function creerSequence(init, step){
+	return() => init+=step;
+}
+
+
+function fibonacci(){
+
+}
+
+function creerMultiplicateurModif(){
+	
+}
