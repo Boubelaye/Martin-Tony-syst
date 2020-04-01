ad9b5fedcc01a3a79041551b0763ee3e420e6dbf
Tony Martin <tonym_10@hotmail.fr>
2020-04-01 09:59:25 +0200

    avancement

--- JS_TD1/ex2.js
+++ JS_TD1/ex2.js
@@ -5,16 +5,42 @@ function creerMultiplicateur(n){
 	return(x) => x*n;
 }
 
+console.log("Multiplicateur de 7 par 3 :"+creerMultiplicateur(7)(3))
+
+
 
 function creerSequence(init, step){
-	return() => init+=step;
+	var acc=init-step;
+
+	return function(){
+		acc+=step;
+		return acc;
+	}
 }
 
+let sequence=creerSequence(6, 4);
+console.log("Sequence : init=6 ; step=4");
+/**à terminer*/
 
-function fibonacci(){
 
+
+function fibonacci(nb1, nb2){
+	let suiteFibo=[nb1, nb2];
+	return()==>{
+		suiteFibo.push(suiteFibo[suiteFibo.length-2]+suiteFibo[suiteFibo.length-1]);
+		return suiteFibo.join('');
+	}
 }
+
+console.log()/**à faire*/
+
+
 
 function creerMultiplicateurModif(){
-	
+
+}
+
+
+function powerModif(){
+
 }
